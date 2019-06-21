33c0ad8f6128f986e8c481e56355fb2f7e713186
santipm29 <santipmartinez@outlook.com>
2019-06-21 08:55:06 -0500

    :tada: first commit

---
+++ app/app.py
@@ -0,0 +1,39 @@
+'''Module main'''
+import json
+from rabbitmq import RabbitMQ
+
+class App:
+    '''clase novedades dependencias'''
+    dependencias = None
+    ibmmqobj = None
+
+    @classmethod
+    def __init__(cls):
+        '''Method init'''
+        cls.config = json.loads(open('config/config.json').read())
+      
+
+    @classmethod
+    def callback(cls, channel, method, properties, body):
+        '''Receive message '''
+        del properties
+        data = json.loads(body.decode('utf-8'))
+        print(data)
+        
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    @classmethod
+    def main(cls):
+        '''start application'''
+        objqueue = RabbitMQ(**cls.config['source'])
+        if not objqueue.connect():
+            raise RuntimeError(['connection_not_established'])
+        objqueue.channel.basic_consume(
+            queue=cls.config['source']['queue'],
+            on_message_callback=cls.callback,
+            auto_ack=False
+        )
+        objqueue.channel.start_consuming()
+
+if __name__ == '__main__':
+    App().main()
